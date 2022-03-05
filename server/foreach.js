a9732e36616aa965ec0c52070a89facbf48064fa
Alejandro González Reyes <alexglzrys@gmail.com>
2022-03-04 19:10:40 -0600

    forEach

---
+++ server/foreach.js
@@ -0,0 +1,4 @@
+const technologies = ['PHP', 'HTML', 'CSS', 'JavaScript', 'MySQL'];
+
+// El método forEach permite recorrer los elementos de un array sin tener que preocuparse por la dimensión del mismo
+technologies.forEach(element => console.log(element));
